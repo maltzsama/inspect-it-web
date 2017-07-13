7a971e25eefdff1474744d0d2315d193ca72da3d
Demetrius Albuquerque <demetrius.albuquerque@yahoo.com.br>
2017-07-12 23:05:30 -0400

    added devise to project

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def flash_class(level)
+    {
+      notice: 'alert alert-info',
+      success: 'alert alert-success',
+      error: 'alert alert-danger',
+      alert: 'alert alert-warning'
+    }.fetch(level.to_sym)
+  end
 end
