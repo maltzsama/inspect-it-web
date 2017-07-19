c8fc54d73958a6b7128732f49598fb2c3d8e7a3e
Demetrius Albuquerque <demetrius.albuquerque@yahoo.com.br>
2017-07-18 23:30:45 -0400

    added company and inspection association

--- app/models/inspection.rb
+++ app/models/inspection.rb
@@ -1,3 +1,4 @@
 class Inspection < ApplicationRecord
     validates :name, presence: true 
+    belongs_to :company
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170719001809) do
+ActiveRecord::Schema.define(version: 20170719032200) do
 
   create_table "companies", force: :cascade do |t|
     t.string "name", default: "", null: false
@@ -27,6 +27,8 @@ ActiveRecord::Schema.define(version: 20170719001809) do
     t.string "name", default: "", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "company_id"
+    t.index ["company_id"], name: "index_inspections_on_company_id"
   end
 
   create_table "users", force: :cascade do |t|
