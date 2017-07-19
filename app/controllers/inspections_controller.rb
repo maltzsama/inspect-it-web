2b7fe2ba3b4a3efaf4a64a409406a1d91515cb80
Demetrius Albuquerque <demetrius.albuquerque@yahoo.com.br>
2017-07-18 21:45:48 -0400

    inialized inspections views

---
+++ app/controllers/inspections_controller.rb
@@ -0,0 +1,20 @@
+class InspectionsController < ApplicationController
+  def index
+  end
+
+  def new
+    @inspection = Inspection.new
+  end
+
+  def destroy
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def create
+  end
+end
