37ad9ae46b6dc5193aa52ecdaba9eae4521496e5
Demetrius Albuquerque <demetrius.albuquerque@yahoo.com.br>
2017-07-25 18:30:34 -0400

    converted files to slim

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170725204753) do
+ActiveRecord::Schema.define(version: 20170725210007) do
 
   create_table "companies", force: :cascade do |t|
     t.string "name", default: "", null: false
@@ -33,8 +33,8 @@ ActiveRecord::Schema.define(version: 20170725204753) do
 
   create_table "regulations", force: :cascade do |t|
     t.string "code"
-    t.string "type"
     t.string "name"
+    t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
