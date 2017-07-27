affc944295f16c9f79da892ede106c7093b01260
Demetrius Albuquerque <demetrius.albuquerque@yahoo.com.br>
2017-07-26 21:57:03 -0400

    authentication suspended

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
 
   devise_for :users
-  authenticate :user do
+  # authenticate :user do
     resources :regulations
     resources :companies, only: [:index, :new, :create, :show, :destroy]
     resources :inspections, only: [:index, :new, :create, :show, :edit, :destroy, :update]
     resources :occurrences
-  end
+  # end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "home#index"
