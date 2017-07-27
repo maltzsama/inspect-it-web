6603be19411b0d16945584730b3c4fb55c886654
Demetrius Albuquerque <demetrius.albuquerque@yahoo.com.br>
2017-07-26 20:05:10 -0400

    updated date format

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
 
-  resources :occurrences
   devise_for :users
   authenticate :user do
     resources :regulations
     resources :companies, only: [:index, :new, :create, :show, :destroy]
     resources :inspections, only: [:index, :new, :create, :show, :edit, :destroy, :update]
+    resources :occurrences
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
